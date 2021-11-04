2821af05bb122ea1d872796ca3545eabacadc2d0
Rodrigo Amorim <rodrigo.amorim@xpi.com.br>
2021-11-04 13:39:11 -0300

    feat: clientController

--- Service/BlueBankAPI/Controllers/ClientController.cs
+++ Service/BlueBankAPI/Controllers/ClientController.cs
@@ -13,10 +13,28 @@ namespace BlueBank.Domain.BlueBankAPI.Controllers
     public class ClientController : ControllerBase
     {
         [HttpGet]
-        public List<Client> Get()
+        public List<Client> GetAll()
         {
             // metodo para acessar o BD e obter a lista de clientes
             return new List<Client> { }; //formatar caso não esteja;          
         }
+
+        [HttpGet("{Id}")]
+        public  IActionResult GetClient([FromRoute]Guid Id)
+        {
+            // metodo para acessar o BD e obter a lista de clientes
+            return Ok(new Client); //formatar caso não esteja;          
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult DeleteClient([FromRoute]Guid Id)
+        {
+           // metodo para acessar o BD e deletar um cliente
+           //Não retorna nada
+           return Ok(); // caso de sucesso
+            return BadRequest();
+        }
+
+
     }
 }
