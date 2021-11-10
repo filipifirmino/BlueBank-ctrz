68a6cfa4aff2789bec8507201c9b18ceac31f156
Filipi Firmino <contatoic3web@gmail.com>
2021-11-10 13:42:14 -0300

    Client Repository

---
+++ Core/Interface/IClientRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBank.Domain.Core.Interface
+{
+    public interface IClientRepository
+    {
+        public List<Client> GetAll();
+
+        public Client GetById(Guid id);
+
+        public void Add(Client client);
+
+        public void Save();
+
+    }
+}
--- Data/Repositorys/ClientRepository.cs
+++ Data/Repositorys/ClientRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BlueBank.Domain.Core;
+using BlueBank.Domain.Core.Interface;
+using BlueBank.Domain.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,34 @@ using System.Threading.Tasks;
 
 namespace Data.Repositorys
 {
-    class ClientRepository
+    public class ClientRepository : IClientRepository
     {
+        private readonly BankContext _bankContext;
+
+        public ClientRepository(BankContext context)
+        {
+            _bankContext = context;
+        }
+        
+        public List<Client> GetAll()
+        {
+            return _bankContext.Clients.AsNoTracking().AsQueryable().ToList();
+        }
+
+        public Client GetById(Guid id)
+        {
+            return _bankContext.Clients.AsNoTracking().Single(c => c.Id == id);
+        }
+
+        public void Add(Client client)
+        {
+            _bankContext.Add(client);
+        }
+
+        public void Save()
+        {
+            _bankContext.SaveChanges();
+        }
+
     }
 }
