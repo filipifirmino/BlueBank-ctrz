73ff82e90521b85ce92a8358058ce5be3a5cc226
ingrideme <55901274+ingrideme@users.noreply.github.com>
2021-11-12 10:41:31 -0300

    Criação Validator

---
+++ Core/Validator/UpdateClientRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using BlueBank.Domain.Core.Requestes;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBank.Domain.Core.Validator
+{
+    public class UpdateClientRequestValidator : AbstractValidator<UpdateClientRequest>
+    {
+        public UpdateClientRequestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().NotNull();
+            RuleFor(x => x.Phone).NotEmpty().NotNull();
+            RuleFor(x => x.Cpf).NotEmpty().NotNull();
+            RuleFor(x => x.Address).NotEmpty().NotNull();
+        }
+
+    {
+    }
+}
