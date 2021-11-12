1d56142bfbbab25972e6d1d84cd56571a3249567
ingrideme <55901274+ingrideme@users.noreply.github.com>
2021-11-12 10:38:05 -0300

    Criação Responses

---
+++ Core/Responses/AddClientResponse.cs
@@ -0,0 +1,21 @@
+﻿using BlueBank.Domain.Shared;
+using BlueBank.Domain.Shared.Responses;
+using BlueBank.Domain.Shared.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBank.Domain.Core.Responses
+{
+    public class AddClientResponse : Response
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public Address Address { get; set; }
+        public string Cpf { get; set; }
+        public AccountType TypeAccount { get; set; }
+    }
+}
---
+++ Core/Responses/UpdateClientResponse.cs
@@ -0,0 +1,15 @@
+﻿using BlueBank.Domain.Shared.Responses;
+using BlueBank.Domain.Shared.ValueObjects;
+using System;
+
+namespace BlueBank.Domain.Core.Responses
+{
+    public class UpdateClientResponse : Response
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public Address Address { get; set; }
+        public string Cpf { get; set; }
+    }
+}
